457739cc97f460702361b317370d5e3e1115c2a5
tanyanzhi <345723624@qq.com>
2015-07-29 22:22:59 +0800

    a

---
+++ Assets/OffLineDocumentation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class OffLineDocumentation : MonoBehaviour {
+
+    public string enPath;
+	void Start () {
+
+        string[] files = Directory.GetFiles(enPath, "*.html", SearchOption.AllDirectories);
+
+        string content;
+
+        foreach (string file in files) {
+
+            content = File.ReadAllText(file);
+
+            content = content.Replace("http://", "");
+
+            File.WriteAllText(file, content);
+
+        }
+        
+	}
+	
+
+}
